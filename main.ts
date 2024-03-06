1a98f90985669a166fb99a933398153336ce0caa
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-06 22:13:27 +0500

    challange days

---
+++ main.ts
@@ -0,0 +1,10 @@
+// 13.	Your Own Array: Think of your favorite mode of transportation, 
+// such as a motorcycle or a car, and make a list that stores several examples. 
+// Use your list to print a series of statements about these items, 
+// such as “I would like to own a Honda motorcycle.”
+let fav_transport = ['Honda motorcycle", "Tesla car", "Bianchi bicycle'];
+console.log(`I would like to own a ${fav_transport[0]} Honda motorcycle.`);
+console.log(`I would like to own a ${fav_transport[1]}  Tesla car`);
+console.log(`I would like to own a ${fav_transport[2]} Bianchi bicycle`);
+
+
